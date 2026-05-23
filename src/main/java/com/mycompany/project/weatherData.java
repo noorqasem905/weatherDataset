bf9495d6272ade8d96db9573abd218ca318924e6
Zaidalqudsi <zaidalqudsi34@gmail.com>
2026-05-23 17:45:33 +0300

    Add files via upload

---
+++ src/main/java/com/mycompany/project/weatherData.java
@@ -0,0 +1,48 @@
+
+package com.mycompany.project;
+
+/**
+ *
+ * @author USER
+ */
+public class weatherData {
+    private String formattedDate;
+    private String summary;
+    private String precipType;
+    private double temperature;
+    private double apparentTemperature;
+    private double humidity;
+    private double windSpeed;
+
+    public weatherData(String formattedDate, String summary, String precipType, 
+                       double temperature, double apparentTemperature, double humidity, double windSpeed) {
+        this.formattedDate = formattedDate;
+        this.summary = summary;
+        this.precipType = precipType;
+        this.temperature = temperature;
+        this.apparentTemperature = apparentTemperature;
+        this.humidity = humidity;
+        this.windSpeed = windSpeed;
+    }
+
+    public String getFormattedDate() { return formattedDate; }
+    public void setFormattedDate(String formattedDate) { this.formattedDate = formattedDate; }
+
+    public String getSummary() { return summary; }
+    public void setSummary(String summary) { this.summary = summary; }
+
+    public String getPrecipType() { return precipType; }
+    public void setPrecipType(String precipType) { this.precipType = precipType; }
+
+    public double getTemperature() { return temperature; }
+    public void setTemperature(double temperature) { this.temperature = temperature; }
+
+    public double getApparentTemperature() { return apparentTemperature; }
+    public void setApparentTemperature(double apparentTemperature) { this.apparentTemperature = apparentTemperature; }
+
+    public double getHumidity() { return humidity; }
+    public void setHumidity(double humidity) { this.humidity = humidity; }
+
+    public double getWindSpeed() { return windSpeed; }
+    public void setWindSpeed(double windSpeed) { this.windSpeed = windSpeed; }
+}
